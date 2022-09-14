74520fa611c36372d6870db891c5e0612a18b138
bbbb <bgh9651@gmail.com>
2022-09-14 20:44:08 +0900

    api key 삭제

--- BGH/wanted-pre-onboarding-things-flow-bgh/src/api/index.js
+++ BGH/wanted-pre-onboarding-things-flow-bgh/src/api/index.js
@@ -3,11 +3,7 @@ import axios from 'axios';
 const owner = 'angular';
 const repo = 'angular-cli';
 const BASE_URL = `https://api.github.com/repos/${owner}/${repo}`;
-const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const instance = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    Authorization: `Bearor ${API_KEY}`,
-  },
 });
